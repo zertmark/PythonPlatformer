7d787c52ed63c54f79c452e016ec70e6d7626155
zertmark <47692978+zertmark@users.noreply.github.com>
2021-09-12 22:21:18 +0300

    Add files via upload

---
+++ src/Animations.py
@@ -0,0 +1,21 @@
+ANIMATION_DELAY = 1
+ANIMATION_RIGHT = ['levels/r1.png',
+                   'levels/r2.png',
+                   'levels/r3.png',
+                   'levels/r4.png',
+                   'levels/r5.png']
+ANIMATION_LEFT = ['levels/l1.png',
+                  'levels/l2.png',
+                  'levels/l3.png',
+                  'levels/l4.png',
+                  'levels/l5.png']
+ANIMATION_JUMP_LEFT = [('levels/jl.png', ANIMATION_DELAY)]
+ANIMATION_JUMP_RIGHT = [('levels/jr.png', ANIMATION_DELAY)]
+ANIMATION_JUMP = [('levels/j.png', ANIMATION_DELAY)]
+ANIMATION_STAY = [('levels/0.png', ANIMATION_DELAY)]
+ANIMATION_BLOCK_TELEPORT = [('levels/portal1.png', ANIMATION_DELAY),
+                            ('levels/portal2.png', ANIMATION_DELAY)]
+ANIMATION_MONSTER = [('levels/fire1.png', ANIMATION_DELAY),
+                     ('levels/fire2.png', ANIMATION_DELAY)]
+ANIMATION_BLOCK_TELEPORT = [('levels/portal1.png', 1),
+                            ('levels/portal2.png', 1)]
